a816fbcf65db1de743376bf4eec6f2a4643f482e
xxiaoling <100384762+xxiaoling@users.noreply.github.com>
2025-02-24 20:09:36 -0500

    fixed backgorund issue

--- PCG project/Assets/Scripts/BackgroundController.cs
+++ PCG project/Assets/Scripts/BackgroundController.cs
@@ -18,8 +18,9 @@ public class BackgroundController : MonoBehaviour
     void FixedUpdate()
     {
         float distance = cam.transform.position.x * parallaxEffect;
-        float movement = cam. transform.position.x * (1 - parallaxEffect);
-        transform.position = Vector3.Lerp(transform.position, new Vector3(camPosition + distance, transform.position.y, transform.position.z), Time.deltaTime * 5f);
+        float movement = cam.transform.position.x * (1 - parallaxEffect);
+
+        transform.position = new Vector3(camPosition + distance, transform.position.y, transform.position.z);
 
         if (movement > camPosition + bgLength) { camPosition += bgLength; }
         else if (movement < camPosition - bgLength) { camPosition -= bgLength; }
